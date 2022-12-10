0f416c60ac02459d0c429751e2cce3fd6d9b5867
JustGui <justin.albinet@gmail.com>
2022-12-10 20:19:47 +0100

    Update movies & add tables

--- api/cinemaccs_project/models/movies.py
+++ api/cinemaccs_project/models/movies.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class Movies(models.Model):
     title = models.CharField(max_length=300, null=True)
     duration = models.CharField(max_length=300, null=True)
-    allocine_id = models.CharField(max_length=10, null=True)
-    isan_id = models.CharField(max_length=50, null=True)
-    poster = models.FileField(null=True)
-    releaste_date = models.DateTimeField(null=True)
+    allocineId = models.CharField(max_length=10, null=True)
+    isanId = models.CharField(max_length=50, null=True)
+    poster = models.ImageField(upload_to="pictures/movies", max_length=255, null=True) 
+    releaseDate = models.DateTimeField(null=True)
     synopsis = models.CharField(max_length=500, null=True)
     director = models.CharField(max_length=300, null=True)
     createdDate = models.DateTimeField(auto_now_add=True, null=True)
