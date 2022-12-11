f3604f8a96f5c60f618e0e0dfeafbcf9d70594c8
yann_dm <yann.montmarin@gmail.com>
2022-12-11 11:56:07 +0100

    WIP: Serializer

--- api/cinemaccs_project/serializers/theater.py
+++ api/cinemaccs_project/serializers/theater.py
@@ -1,19 +1,27 @@
 from rest_framework import serializers
 
 from cinemaccs_project.models import Theater
+from cinemaccs_project.serializers import TheaterPictureSerializer
 
 
-class TheaterListSerializer(serializers.HyperlinkedModelSerializer):
+class TheaterSerializer(serializers.ModelSerializer):
+    full_name = serializers.Field(read_only=True)
+    address = serializers.Field(read_only=True)
+    pictures = TheaterPictureSerializer(many=True)
+    # Render accessibility text
+
+    class Meta:
+        model = Theater
+        fields = "__all__"
+
+
+class TheaterListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Theater
         fields = [
-            "name",
-            "zipcode"
+            "full_name",
+            "address",
+            "zipcode",
+            "logo"
         ]
-
-
-class TheaterRetrieveSerializer(serializers.ModelSerializer):
-    class Meta:
-        # TODO
-        model = Theater
-        fields = ["name", "zipcode"]
