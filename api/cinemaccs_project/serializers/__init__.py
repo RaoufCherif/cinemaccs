eb690599f3bfdfea60b3c2a631a74558a1227560
yann_dm <yann.montmarin@gmail.com>
2023-07-22 00:54:05 +0200

    Change serializers

--- api/cinemaccs_project/serializers/__init__.py
+++ api/cinemaccs_project/serializers/__init__.py
@@ -1,15 +1,21 @@
 from .mixins import ExtraFieldMixin
-
 from .brand import BrandSerializer
 from .description_element import DescriptionElementSerializer
+from .room import RoomSerializerNoTheater, RoomSerializer
+from .room_picture import RoomPictureSerializer
+from .theater import (
+    TheaterSerializer, TheaterSerializerNoRooms,
+    TheaterListSerializer
+)
 from .theater_picture import TheaterPictureSerializer
-from .theater import TheaterSerializer, TheaterListSerializer
 
 
 __all__ = [
     "BrandSerializer",
     "DescriptionElementSerializer",
-    "TheaterPictureSerializer",
+    "RoomSerializer",
+    "RoomPictureSerializer",
     "TheaterSerializer",
     "TheaterListSerializer",
+    "TheaterPictureSerializer"
 ]
